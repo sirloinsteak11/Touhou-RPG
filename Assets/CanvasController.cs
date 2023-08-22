72428f6ee6783bd0dac92a479f5f90716ad0bd55
Deonn Almirol <deonn.almirol@gmail.com>
2023-08-21 17:05:49 -0700

    new screens added

--- Assets/CanvasController.cs
+++ Assets/CanvasController.cs
@@ -5,26 +5,97 @@ using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour
 {
-    [SerializeField] GameObject MainMenu, FightMenu;
-    [SerializeField] Button PlayButton;
+    [SerializeField] Button PlayButton, FightButton, GachaButton;
     public AudioSource musicSource;
-    public AudioClip mmBGM, fightBGM;
+    public AudioClip mmBGM, fightBGM, CharaSelBGM, GachaBGM;
+    [SerializeField] Button[] BackButtons;
+
+    private string LastMenu;
+    private AudioClip LastBGM;
 
     void Start()
     {
-        PlayButton.onClick.AddListener(SwitchMenu);
+        PlayButton.onClick.AddListener(PlayButtonListener);
+        FightButton.onClick.AddListener(FightButtonListener);
+        GachaButton.onClick.AddListener(GachaButtonListener);
+
+        for (int i = 0; i < BackButtons.Length; i++)
+        {
+            int closureIndex = i;
+            BackButtons[closureIndex].onClick.AddListener(GoBackMenu);
+        }
+
         musicSource.clip = mmBGM;
         musicSource.Play();
     }
 
-    void SwitchMenu()
+    void PlayButtonListener()
     {
-        if (MainMenu.activeInHierarchy)
+        foreach (Transform child in transform)
         {
-            MainMenu.SetActive(false);
-            FightMenu.SetActive(true);
-            musicSource.clip = fightBGM;
-            musicSource.Play();
+            if (child.gameObject.activeInHierarchy)
+            {
+                LastMenu = child.name;
+                LastBGM = musicSource.clip;
+            }
+
+            child.gameObject.SetActive(false);
         }
+
+        gameObject.transform.Find("CharacterSelectCanvas").gameObject.SetActive(true);
+        musicSource.clip = CharaSelBGM;
+        musicSource.Play();
+        Debug.Log($"{LastMenu}, {LastBGM}");
+    }
+
+    void FightButtonListener()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.activeInHierarchy)
+            {
+                LastMenu = child.name;
+                LastBGM = musicSource.clip;
+            }
+
+            child.gameObject.SetActive(false);
+        }
+
+        gameObject.transform.Find("FightCanvas").gameObject.SetActive(true);
+        musicSource.clip = fightBGM;
+        musicSource.Play();
+        Debug.Log($"{LastMenu}, {LastBGM}");
+    }
+
+    void GachaButtonListener()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.activeInHierarchy)
+            {
+                LastMenu = child.name;
+                LastBGM = musicSource.clip;
+            }
+
+            child.gameObject.SetActive(false);
+        }
+
+        gameObject.transform.Find("GachaMenuCanvas").gameObject.SetActive(true);
+        musicSource.clip = GachaBGM;
+        musicSource.Play();
+        Debug.Log($"{LastMenu}, {LastBGM}");
+    }
+
+    void GoBackMenu()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+        gameObject.transform.Find(LastMenu).gameObject.SetActive(true);
+        musicSource.clip = LastBGM;
+        musicSource.Play();
+        Debug.Log("back button pressed " + LastMenu + " " + LastBGM);
     }
 }
